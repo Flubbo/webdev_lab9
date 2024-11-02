f941d01d356e7219c752a2a8c9347fe57effca0d
Flubbo <lishaidan@gmail.com>
2024-11-02 16:55:27 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function sendMessage(){
-  var msg = document.getElementById('Aidan Lish:'+'entryinput');
-  alert(msg);
+  var msg = document.getElementById('entryinput');
+  alert('Aidan Lish'+msg);
 }
 
 var alertButton = document.getElementById('entrybutton');
