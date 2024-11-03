8c925c72d0c1c43a394bdfc20e1be6fb9a30abdb
Flubbo <lishaidan@gmail.com>
2024-11-02 17:10:14 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 function sendMessage(){
-  var msg = document.getElementById('entryinput');
-  alert('Aidan Lish'+msg);
+  alert('Aidan Lish:'+ msg.value);
 }
 
 var alertButton = document.getElementById('entrybutton');
-alertButton.addEventListener('click', sendMessage)
+var output = document.getElementById('textoutput');
+var msg = document.getElementById('entryinput');
+alertButton.addEventListener('click', sendMessage);
+alertButton.addEventListener('click', function(){
+  output.innerHTML = msg.value;
+});
